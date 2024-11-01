0d704a28589ad4494057e4cd9db6f5b325f61da1
Ghaliah <ghalia.banouna@gmail.com>
2024-11-01 18:31:53 +0300

    Edit on light mode and dark mode looks

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -7,7 +7,15 @@ def mainView(request: HttpRequest):
 
 #Property page
 def propertyView(request: HttpRequest):
-    return render(request, "main/property.html")
+
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+
+    return render(request, "main/property.html", context={"properties" : properties})
 
 #Contact us page
 def contactView(request: HttpRequest):
