3417834c9e550061c1ff8faf08d3b3164a5ef8b8
KhuloodGhazi <khulood.gmo@gmail.com>
2024-11-01 19:33:28 +0300

    Finish Properties & Contact Pages

--- RealEstate/RealEstate/settings.py
+++ RealEstate/RealEstate/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -9,9 +9,15 @@ def main_view(request:HttpRequest):
 
 
 
-def propertie_view(request:HttpRequest):
+def propertie_view(request):
 
-    return render(request, "main/properties.html")
+    properties = [
+        {"title": "Villa Modern in Malqa", "image": "villa1.jpg"},
+        {"title": "Great home for you in Rimal", "image": "villa2.jpg"},
+        {"title": "Villa with 8 bedrooms in Swedey", "image": "villa3.jpg"},
+        {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
+    ]
+    return render(request, 'main/properties.html', {'properties': properties})
 
 
 
