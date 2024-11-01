e2125b3ed8c5eb9ce1092a6ea141b58dc8bcc2c6
HmzhDubh <meezo.2015@hotmail.com>
2024-11-01 05:50:47 +0300

    complete dark mode

--- Cookies/cookiesApp/urls.py
+++ Cookies/cookiesApp/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("contact/", views.contact_view, name="contact_view"),
     path("services/", views.contact_view, name="services_view"),
     path("about/", views.contact_view, name="about_view"),
+    path("mode/dark", views.dark_mode, name="dark_mode_view"),
+    path("mode/light", views.light_mode, name="light_mode_view"),
 
 ]
--- Cookies/cookiesApp/views.py
+++ Cookies/cookiesApp/views.py
@@ -3,33 +3,42 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 
-
 def main_view(request:HttpRequest):
     request = render(request, 'main.html')
     return request
 
-def properties_view(request:HttpRequest):
+
+def properties_view(request: HttpRequest):
 
     request = render(request, 'properties.html')
     return request
 
-def contact_view(request:HttpRequest):
+
+def contact_view(request: HttpRequest):
 
     request = render(request, 'contact.html')
     return request
 
-def dark_mode(request:HttpRequest):
-    request = redirect("{% url 'cookiesApp:main_view' %}")
-    request.set_cookie("mode", "dark")
+
+def dark_mode(request: HttpRequest):
+    request = redirect('cookiesApp:main_view')
+    request.set_cookie("mode", "dark", max_age=60*60*24)
+    print(request.cookies)
+    return request
+
+def light_mode(request: HttpRequest):
+    request = redirect('cookiesApp:main_view')
+    request.delete_cookie("mode")
+    print(request.cookies)
     return request
 
 
-def services_view(request:HttpRequest):
+def services_view(request: HttpRequest):
     request = render(request, "services.html")
 
     return request
 
-def about_view(request:HttpRequest):
+def about_view(request: HttpRequest):
     request = render(request, "about.html")
 
     return request
