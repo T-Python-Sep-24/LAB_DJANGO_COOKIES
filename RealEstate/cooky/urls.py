fea3d1b43bdbc13e58b70ac60ac52da4da6c1bcd
osama_fahad <osamaf.net@gmail.com>
2024-11-03 00:58:01 +0300

    Done from the lab

--- RealEstate/cooky/urls.py
+++ RealEstate/cooky/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("properties/", views.property_view, name="property_view"),
     path("contact/", views.contact_view, name="contact_view"),
-    path("mode/dark/", views.dark_view, name="dark_view")
+    path("mode/dark/", views.dark_view, name="dark_view"),
+    path("mode/light/", views.light_view, name="light_view")
 ]
--- RealEstate/cooky/views.py
+++ RealEstate/cooky/views.py
@@ -5,12 +5,24 @@ def home_view(request: HttpRequest):
     return render(request, "cooky/home.html")
 
 def property_view(request: HttpRequest):
-    return render(request, "cooky/property.html")
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+    
+    return render(request, "cooky/property.html", context={"properties": properties})
 
 def contact_view(request: HttpRequest):
     return render(request, "cooky/contact.html")
 
 def dark_view(request: HttpRequest):
-    response = redirect("cooky:home_view")
-    response.set_cookie("mode", "dark", max_age=60)
+    response = redirect(request.GET.get("next", "/"))
+    response.set_cookie("mode", "dark", max_age=60*3)
+    return response
+
+def light_view(request: HttpRequest):
+    response = redirect(request.GET.get("next", "/"))
+    response.set_cookie("mode", "light", max_age=-180)
     return response
