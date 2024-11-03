7a6130220ca453378ad00604a4b79fee7a8955bd
Khalid ALMaghyadi <K95.job@gmail.com>
2024-11-03 19:45:13 +0300

    Done

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = "main"
 # راح ارجعله 
 urlpatterns = [
-    path('',views.home_light_view , name='home_light_view'),
-    path('home/dark',views.home_dark_view , name='home_dark_view')
+
+
+    path('',views.home_view , name='home_view'),
+    path('home/light',views.home_light_view , name="home_light_view" ),
+    path('home/dark',views.home_dark_view , name='home_dark_view'),
+    path('properties/',views.properties_view , name='properties_view'),
     
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -3,18 +3,29 @@ from django.shortcuts import render,redirect
 # Create your views here.
 
 from django.http import HttpRequest , HttpResponse 
+def home_view(request:HttpRequest):
+    return render(request , 'main/home.html')
 
 
 def home_light_view(request:HttpRequest):
     
-    # return render(request ,"main/homelight.html")
-    response1 = render(request, 'main/homelight.html')
-    response1.set_cookie('mode', 'light', max_age=60*60*24*7)  # Cookie expires in 7 days
-    return response1
+    response = redirect('main:home_view')
+    response.set_cookie('mode', 'light', max_age=-23324)  # Cookie expires in 7 days
+    return response
 
 def home_dark_view(request:HttpRequest):
 
     # return render(request , "main/homedark.html")
-    response = render(request, 'main/homedark.html')  # You can change this if you have a specific dark mode template
+    response = redirect('main:home_view')  # You can change this if you have a specific dark mode template
     response.set_cookie('mode', 'dark', max_age=60*60*24*7)
     return response
+
+def properties_view(request:HttpRequest):
+     properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+     return render(request, "main/properties.html", context={"properties" : properties})
+
