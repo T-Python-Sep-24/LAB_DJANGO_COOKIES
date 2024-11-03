7edaf820a9608797150c2a8f12768ca37e48044b
AR <aarokan@gmail.com>
2024-11-03 06:43:22 +0300

    Solve Real Estate Lab

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.home_view, name = "home_view")
+    path("", views.home_view, name = "home_view"),
+    path("properties/", views.properties_view, name = "properties_view"),
+    path("contact/", views.contact_view, name = "contact_view"),
+    path("dark/mode/", views.dark_mode_view, name = "dark_mode_view"),
+    path("light/mode/", views.light_mode_view, name = "light_mode_view"),
 ]
